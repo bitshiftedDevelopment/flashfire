004378b2dfd05490c38459f19bac83f94ed14829
Mick Evans <bitshifted.development@gmail.com>
2018-04-05 13:26:15 -0400

    chore: added database module and imported into flashfire module

--- src/app/flashfire/flashfire.module.ts
+++ src/app/flashfire/flashfire.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthModule } from './auth/auth.module';
+import { FiredbModule } from './firedb/firedb.module';
 @NgModule({
   imports: [
     CommonModule,
-    AuthModule
+    AuthModule,
+    FiredbModule
   ],
   declarations: []
 })
