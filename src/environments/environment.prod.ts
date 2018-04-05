938acc97b7aee4a853002fa7836153d5fece2507
Mick Evans <bitshifted.development@gmail.com>
2018-04-05 12:10:39 -0400

    chore: added firebase config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBiCxZjK_OKnEbCyBM9Y9Rb5FF-w8fIJ0w",
+    authDomain: "flashfire-d6588.firebaseapp.com",
+    databaseURL: "https://flashfire-d6588.firebaseio.com",
+    projectId: "flashfire-d6588",
+    storageBucket: "flashfire-d6588.appspot.com",
+    messagingSenderId: "351573906093"
+  }
+};
